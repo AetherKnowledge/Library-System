014f1bdca047838a0af08b935b34ddd52fda482b
AetherKnowledge <152044019+AetherKnowledge@users.noreply.github.com>
2024-05-24 00:07:08 +0800

    Bug Fixes - Panels resizing properly

--- src/main/java/com/librarysystem/panels/books/IssuedBooks.java
+++ src/main/java/com/librarysystem/panels/books/IssuedBooks.java
@@ -129,7 +129,7 @@ public class IssuedBooks extends MyPanel {
             jPanel1.repaint();
         }
         
-        jPanel11.setPreferredSize(new Dimension(870, columnHeight));
+        jPanel11.setPreferredSize(new Dimension(jPanel1.getWidth() - 5, columnHeight));
         issuedList.setSize(new Dimension(jPanel11.getWidth(), columnHeight));
         layout.setRows(issuedBooks.size());
     }    
